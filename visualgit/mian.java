0ab62a04ed43acf9bb4e7a072325c0797b941013
ZraryZU <zurui@shu.edu.cn>
2023-03-01 17:58:39 +0800

    push调好了

--- visualgit/mian.java
+++ visualgit/mian.java
@@ -26,7 +26,7 @@ public class mian {
         System.out.println(git);
 //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
         CredentialsProvider provider = new UsernamePasswordCredentialsProvider("PRIVATE-TOKEN", "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");  //生成身份信息
-        //CredentialsProvider provider = new UsernamePasswordCredentialsProvider("ZraryZU", "git5417..");  //生成身份信息
+
         String branch = "master";   //克隆目标分支
         git.push()
                 .setRemote("origin")    //设置推送的URL名称
